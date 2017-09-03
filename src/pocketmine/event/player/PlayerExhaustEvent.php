cc9023e247e52d8412d41cffe5fdc5822cd4d960
Infernus <Infernus101@users.noreply.github.com>
2017-09-03 23:25:46 +0530

    Update PlayerExhaustEvent.php
    Food tick when sprinting

--- src/pocketmine/event/player/PlayerExhaustEvent.php
+++ src/pocketmine/event/player/PlayerExhaustEvent.php
@@ -34,16 +34,16 @@ class PlayerExhaustEvent extends PlayerEvent implements Cancellable {
 	const CAUSE_HEALTH_REGEN = 4;
 	const CAUSE_POTION = 5;
 	const CAUSE_WALKING = 6;
-	const CAUSE_SNEAKING = 7;
+	const CAUSE_SPRINTING = 7;
 	const CAUSE_SWIMMING = 8;
-	const CAUSE_JUMPING = 10;
+	const CAUSE_JUMPING = 9;
+	const CAUSE_SPRINT_JUMPING = 10;
 	const CAUSE_CUSTOM = 11;
-
-	const CAUSE_FLAG_SPRINT = 0x10000;
-
 	/** @var float */
 	private $amount;
-
+	/** @var int */
+	private $cause;
+	
 	/**
 	 * PlayerExhaustEvent constructor.
 	 *
@@ -54,6 +54,7 @@ class PlayerExhaustEvent extends PlayerEvent implements Cancellable {
 	public function __construct(Human $human, float $amount, int $cause){
 		$this->player = $human;
 		$this->amount = $amount;
+		$this->cause = $cause;
 	}
 
 	/**
@@ -75,5 +76,9 @@ class PlayerExhaustEvent extends PlayerEvent implements Cancellable {
 	 */
 	public function setAmount(float $amount){
 		$this->amount = $amount;
+	}
+
+	public function getCause() : int{
+		return $this->cause;
 	}
 }
